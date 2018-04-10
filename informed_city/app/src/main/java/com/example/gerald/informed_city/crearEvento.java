ee904d78d2c53ff7957aa281617f3b3123f3d04b
reyesm2 <reyes.marlon57@gmail.com>
2018-04-09 22:34:52 -0600

    marlon_activities

---
+++ informed_city/app/src/main/java/com/example/gerald/informed_city/crearEvento.java
@@ -0,0 +1,13 @@
+package com.example.gerald.informed_city;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class crearEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_evento);
+    }
+}
---
+++ informed_city/app/src/main/java/com/example/gerald/informed_city/eventosFuturos.java
@@ -0,0 +1,13 @@
+package com.example.gerald.informed_city;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class eventosFuturos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventos_futuros);
+    }
+}
