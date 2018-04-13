b895cde265ba08ddada6d0188fc8ed231b500384
reyesm2 <reyes.marlon57@gmail.com>
2018-04-12 22:40:50 -0600

    MapaUbicacionActual

--- informed_city/app/src/main/java/com/example/gerald/informed_city/MainActivity.java
+++ informed_city/app/src/main/java/com/example/gerald/informed_city/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.gerald.informed_city;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapa = findViewById(R.id.btMapa);
+        mapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent mapa = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(mapa);
+            }
+        });
     }
 }
