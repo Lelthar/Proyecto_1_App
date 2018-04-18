70463d240bb3a48cfe515c9f2c3fe4b2f254f7b1
reyesm2 <reyes.marlon57@gmail.com>
2018-04-18 00:43:37 -0600

    registro-SinBackend

--- informed_city/app/src/main/java/com/example/gerald/informed_city/SignUp.java
+++ informed_city/app/src/main/java/com/example/gerald/informed_city/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.gerald.informed_city;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,8 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         botonGuardar = findViewById(R.id.btRegisSU);
+
+
     }
     public  void registrarUsuario(View view){
         EditText correo = findViewById(R.id.edtemailSU);
@@ -23,10 +26,19 @@ public class SignUp extends AppCompatActivity {
         String correoValor = correo.getText().toString();
         String contra1Valor = contra1.getText().toString();
         String contra2Valor = contra2.getText().toString();
-        if(!correoValor.equals("") && !contra1Valor.equals("") && contra2Valor.equals("")){
-
+        if(!correoValor.equals("") && !contra1Valor.equals("") && !contra2Valor.equals("")){
+            if(contra1Valor.equals(contra2Valor)){
+                Intent intent = new Intent(SignUp.this,AjustesCuenta.class);
+                intent.putExtra("intent",1);  //Numero del intent que lo invoca.
+                intent.putExtra("correo",correoValor);
+                intent.putExtra("contra",contra1Valor);
+                startActivity(intent);
+            }else{
+                Toast.makeText(this,"No coincide la contraseña",Toast.LENGTH_SHORT).show();
+            }
         }else{
             Toast.makeText(this,"Datos incompletos",Toast.LENGTH_SHORT).show();
         }
     }
+
 }
