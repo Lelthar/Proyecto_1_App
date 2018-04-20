dac1c0b22db3dbca943da1faf5509e643ce14475
Bryan Hernández Sibaja <bryan_2504@hotmail.com>
2018-04-19 23:48:52 -0600

    avance crear evento

--- informed_city/app/src/main/java/com/example/gerald/informed_city/crearEvento.java
+++ informed_city/app/src/main/java/com/example/gerald/informed_city/crearEvento.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class crearEvento extends AppCompatActivity {
 
@@ -11,5 +16,18 @@ public class crearEvento extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_evento);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerCategoria);
+        String[] datos = new String[] {"Robo o Asalto", "Accidente de Tránsito", "Congestión Vial", "Descarrilamiento de Tren", "Incendio", "Personas Misteriosas en la Zona", "Pleitos o Peleas",
+                "Derrumbes", "Inundaciones", "Caída de Objeto", "Persona Desaparecida", "Mascota Desaparecida", "Apagón", "Sin Agua Potable", "Espectáculo en Vía Pública", "Bloqueo de Vía", "Otros"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, datos);
+        spinner.setAdapter(adapter);
+
+
+    }
+
+    public void btnUbicacion(View view) {
+        Intent intent = new Intent(crearEvento.this, MapaCrear.class);
+        startActivity(intent);
     }
 }
