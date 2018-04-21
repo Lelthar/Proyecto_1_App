6827ca9853c422c2b20ec56d1aa4ffdba3683b84
Bryan Hernández Sibaja <bryan_2504@hotmail.com>
2018-04-20 20:14:07 -0600

    contactenos
    crea un intent hacia el correo electrónico

--- informed_city/app/src/main/java/com/example/gerald/informed_city/AcercaDe.java
+++ informed_city/app/src/main/java/com/example/gerald/informed_city/AcercaDe.java
@@ -1,7 +1,10 @@
 package com.example.gerald.informed_city;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AcercaDe extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class AcercaDe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+    }
+
+    public void Contactar(View view){
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto","admi@informedcity.com", null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "");
+        startActivity(Intent.createChooser(emailIntent, "Send email..."));
+
+
     }
 }
